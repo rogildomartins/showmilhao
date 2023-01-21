de10fa3190e4ed76d76ddd29f1aad220471a85b9
Manoel Rogildo <manoelrogildo@gmil.com>
2023-01-21 09:34:50 -0300

    criando a classe de teste de insert

---
+++ showmilhao/src/main/java/br/com/showmilhao/application/InsertDataForTesting.java
@@ -0,0 +1,42 @@
+package br.com.showmilhao.application;
+
+public class InsertDataForTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+// adicionar objeto
+
+//		Pergunta pergunta = new Pergunta();
+//		pergunta.setNivel("dificil");
+//		pergunta.setEnunciado("Quem descobril o Brasil?");
+//		pergunta.setAlternativa1("Xuxa");
+//		pergunta.setAlternativa2("Tadeu");
+//		pergunta.setAlternativa3("Ana");
+//		pergunta.setResposta("Pedro A. ");		
+//		
+//		perguntaDAO.adcionar(pergunta);
+//		
+
+//		atualizar objeto
+
+//		Pergunta pergunta = new Pergunta();
+//		pergunta.setNivel("dificil");
+//		pergunta.setEnunciado("Quem descobril o Brasil?");
+//		pergunta.setAlternativa1("Xuxa");
+//		pergunta.setAlternativa2("Tadeu");
+//		pergunta.setAlternativa3("Ana");
+//		pergunta.setResposta("Pedro A. ");
+//		pergunta.setId(1);		
+//		
+//		perguntaDAO.atualizar(pergunta);
+
+//		exluir o objeto
+
+//		perguntaDAO.remover(3);	
+
+		System.exit(0);
+
+	}
+
+}
